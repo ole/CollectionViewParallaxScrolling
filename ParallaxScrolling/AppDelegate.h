d68dac6418aa804265713f3110b0faf563ddc45a
Ole Begemann <ole@oleb.net>
2014-05-01 15:32:17 +0200

    Create blank iOS project

---
+++ ParallaxScrolling/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParallaxScrolling
+//
+//  Created by Ole Begemann on 01.05.14.
+//  Copyright (c) 2014 Ole Begemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ParallaxScrolling/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParallaxScrolling
+//
+//  Created by Ole Begemann on 01.05.14.
+//  Copyright (c) 2014 Ole Begemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
